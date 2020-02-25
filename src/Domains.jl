7b3a237194dff7ea61a269d1c73b8e1edabc1005
Roel Stappers <roel.stappers@met.no>
2020-02-25 18:51:51 +0100

    Update Domains.jl

--- src/Domains.jl
+++ src/Domains.jl
@@ -1,10 +1,13 @@
 
 module Domains
 
+using Unmarshal, Glob,JSON
 
-# This struct is not used at the moment 
-# Could parse JSON to struct
+export readdomain
+
 struct Domain     
+    NAME::String
+    TSTEP::Int
     NLON::Int
     NLAT::Int
     LONC::Float64
@@ -15,6 +18,27 @@ struct Domain
     EZONE::Int   
 end
 
+const moduledir = dirname(pathof(Domains))
+const jsondir = joinpath(moduledir, "json")
+const jsonschemafile = joinpath(moduledir , "jsonschema/domain.schema.json")
+
+# const domains = [unmarshal(Domain, JSON.parsefile(f)) for f in Glob.glob("*.json",jsondir)] 
+
+const domains = getindex.(splitext.(basename.(Glob.glob("*.json",jsondir))),1)
+
+readdomain(domainname) =  JSON.parsefile(joinpath(jsondir, "$domainname.json"))        
+
+# This struct is not used at the moment 
+# Could parse JSON to struct
+
+
+
+"""
+    Returns true if [lon,lat] is inside domain 
+"""
+#function ∈(lonlat,d::Domain) = 
+
+#end
 
 end #module Domains
 

f23c35307209bd03905a255fb4e38a2c6715ae16
Roel Stappers <roel.stappers@met.no>
2020-02-26 14:34:52 +0100

    Code restucture

--- src/Domains.jl
+++ src/Domains.jl
@@ -1,9 +1,9 @@
 
 module Domains
 
-using Unmarshal, Glob,JSON
-
-export readdomain
+using Unmarshal, Glob,JSON, Proj4
+import Base
+export readdomain, Domain, in, lonlat2lcc, lcc2lonlat
 
 struct Domain     
     NAME::String
@@ -21,24 +21,53 @@ end
 const moduledir = dirname(pathof(Domains))
 const jsondir = joinpath(moduledir, "json")
 const jsonschemafile = joinpath(moduledir , "jsonschema/domain.schema.json")
+const Rearth = 6.37122e6
 
 # const domains = [unmarshal(Domain, JSON.parsefile(f)) for f in Glob.glob("*.json",jsondir)] 
 
+# domains is an array with domain filenames 
 const domains = getindex.(splitext.(basename.(Glob.glob("*.json",jsondir))),1)
 
-readdomain(domainname) =  JSON.parsefile(joinpath(jsondir, "$domainname.json"))        
+"""
+    readdomain(domainname)
 
-# This struct is not used at the moment 
-# Could parse JSON to struct
-
-
+    returns a `Domain` 
 
 """
-    Returns true if [lon,lat] is inside domain 
-"""
-#function ∈(lonlat,d::Domain) = 
+readdomain(domainname) =  unmarshal(Domain,JSON.parsefile(joinpath(jsondir, "$domainname.json")))
 
-#end
+
+
+function lonlat2lcc(d,lonlat)     
+
+    nlon, nlat   = d.NLON, d.NLAT
+    lon0, lat0   = d.LON0, d.LAT0
+    lonc, latc   = d.LONC, d.LATC
+    gsize        = d.GSIZE
+    ezone        = d.EZONE
+    Plonlat = Proj4.Projection("+proj=longlat +R=$Rearth")
+    Plcc    = Proj4.Projection("+proj=lcc     +R=$Rearth +lat_0=$lat0 +lon_0=$lon0 +lat_1=$lat0 +lat_2=$lat0 +a=$Rearth +b=$Rearth") 
+
+    return Proj4.transform(Plonlat, Plcc,  lonlat)
+end 
+
+function lcc2lonlat(d,xy)    
+
+    nlon, nlat   = d.NLON, d.NLAT
+    lon0, lat0   = d.LON0, d.LAT0
+    lonc, latc   = d.LONC, d.LATC
+    gsize        = d.GSIZE
+    ezone        = d.EZONE
+    Plonlat = Proj4.Projection("+proj=longlat +R=$Rearth")
+    Plcc    = Proj4.Projection("+proj=lcc     +R=$Rearth +lat_0=$lat0 +lon_0=$lon0 +lat_1=$lat0 +lat_2=$lat0 +a=$Rearth +b=$Rearth") 
+
+    return Proj4.transform(Plcc, Plonlat,  xy)
+
+end 
+
+
+
+include("in.jl")
 
 end #module Domains
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,53 +1,28 @@
 
 
 using Test
-using Domains, JSON, Proj4, JSONSchema
+using Domains, JSONSchema,JSON
 
 
 # Validate domain against schema file
 @testset "JSONSchema" begin   
     schema = JSONSchema.Schema(read(Domains.jsonschemafile, String), parentFileDirectory = dirname(Domains.jsonschemafile)) 
-    for domain in Domains.domains  
-        d = readdomain(domain)
+    for domainname in Domains.domains  
+        d = JSON.parsefile(joinpath(Domains.jsondir, "$domainname.json"))
         @test JSONSchema.isvalid(d, schema) 
     end 
-end
+end # @testset "JSONSchema" begin   
 
-# Test that the North Pole is outside the domain. 
-@testset "North Pole" begin      
-    Rearth = 6.37122e6
-
+# Test that the North Pole is outside the domain with ezone. 
+@testset "North Pole" begin          
     # For now exclude non lambert domains from test    
     for domain in filter(d->d ∉ ["SCANDINAVIA_ROTM","NORWAY_POLAR", "RCR_POLAR"], Domains.domains)     
         d = readdomain(domain) 
 
-        nlon, nlat   = d["NLON"], d["NLAT"]
-        lon0, lat0   = d["LON0"], d["LAT0"]
-        lonc, latc   = d["LONC"], d["LATC"]
-        gsize        = d["GSIZE"]
-        ezone        = get(d, "EZONE", 11)   # Perhaps include ezone in json files 
+        # North Pole
+        lonlatNP = [0, 90]
 
-        Plonlat = Proj4.Projection("+proj=longlat +R=$Rearth")
-        Plcc    = Proj4.Projection("+proj=lcc     +R=$Rearth +lat_0=$lat0 +lon_0=$lon0 +lat_1=$lat0 +lat_2=$lat0 +a=$Rearth +b=$Rearth") 
-        
-        lonlat2lcc(lonlat) = Proj4.transform(Plonlat, Plcc,  lonlat)
-        lcc2lonlat(xy)     = Proj4.transform(Plcc, Plonlat,  xy)
-
-        (xc, yc)   = lonlat2lcc([lonc,latc])
-        
-        
-
-        xl  = xc - gsize * (nlon - ezone - 1) / 2
-        xr  = xc + gsize * (nlon - ezone - 1) / 2
-        yt  = yc + gsize * (nlat - ezone - 1) / 2
-        yb  = yc - gsize * (nlat - ezone - 1) / 2
-        xre = xr + gsize * ezone
-        yte = yt + gsize * ezone
-
-        # North pole
-        (xn, yn) = lonlat2lcc([ 0 , 90])        
-
-        @test !((xl < xn < xre) & (yb < yn < yte ))
+        @test !in(lonlatNP,d, with_ezone=true)
     end 
 end # @testset "North Pole" begin 
 

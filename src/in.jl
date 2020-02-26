aaab0e1fd97b4d1bd32bab56afdfa3366667285a
Roel Stappers <roel.stappers@met.no>
2020-02-26 15:22:58 +0100

    Update in.jl

--- src/in.jl
+++ src/in.jl
@@ -6,12 +6,11 @@
 
 Returns true if `lonlat` is inside Domain `d`
 """
-function Base.in(lonlat, d::Domain; with_ezone::Bool = false) 
-    
-   
+function Base.in(lonlat, d::Domain; with_ezone::Bool = false)     
+    v = get_lcc_val(d::Domain)
 
     (xn, yn) = lonlat2lcc(d, lonlat) 
 
-    val = with_ezone ? (xl < xn < xre) && (yb < yn < yte) :  (xl < xn < xr) && (yb < yn < yt)
+    val = with_ezone ? (v.xl < xn < v.xre) && (v.yb < yn < v.yte) :  (v.xl < xn < v.xr) && (v.yb < yn < v.yt)
     return val 
 end 

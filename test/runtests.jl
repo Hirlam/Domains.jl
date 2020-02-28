25e0be904c0a6ccf5c3084b9331016eb0169e766
Roel Stappers <roel.stappers@met.no>
2020-02-28 10:30:42 +0100

    Group test by domain

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,22 @@
 
 
 using Test
-using Domains, JSONSchema,JSON
+using Domains, JSONSchema,JSON, Unmarshal
 
+schema = JSONSchema.Schema(read(Domains.jsonschemafile, String), parentFileDirectory = dirname(Domains.jsonschemafile)) 
 
-# Validate domain against schema file
-@testset "JSONSchema" begin   
-    schema = JSONSchema.Schema(read(Domains.jsonschemafile, String), parentFileDirectory = dirname(Domains.jsonschemafile)) 
-    for domainname in Domains.domains  
-        d = JSON.parsefile(joinpath(Domains.jsondir, "$domainname.json"))
+# For now exclude non lambert domains from test 
+for domain in filter(d->d ∉ ["SCANDINAVIA_ROTM","NORWAY_POLAR", "RCR_POLAR"], Domains.domains)     
+    @testset "$domain" begin 
+        # Validate agains JSON schema
+        d = JSON.parsefile(joinpath(Domains.jsondir, "$domain.json"))    
         @test JSONSchema.isvalid(d, schema) 
-    end 
-end # @testset "JSONSchema" begin   
 
-# Test that the North Pole is outside the domain with ezone. 
-@testset "North Pole" begin          
-    # For now exclude non lambert domains from test    
-    for domain in filter(d->d ∉ ["SCANDINAVIA_ROTM","NORWAY_POLAR", "RCR_POLAR"], Domains.domains)     
-        d = readdomain(domain) 
-
+        # Test that the North Pole is outside the domain with ezone. 
         # North Pole
         lonlatNP = [0, 90]
-
-        @test !in(lonlatNP,d, with_ezone=true)
+        @test !in(lonlatNP, unmarshal(Domain, d), with_ezone = true)
     end 
-end # @testset "North Pole" begin 
 
-  
-
+end # @testset "JSONSchema" begin   
 

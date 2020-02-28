1cf22644e4fb0d140f796b48e19a8ac1b0376d0b
Roel Stappers <roel.stappers@met.no>
2020-02-28 13:46:06 +0100

    clean up test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,18 +5,20 @@ using Domains, JSONSchema,JSON, Unmarshal
 
 schema = JSONSchema.Schema(read(Domains.jsonschemafile, String), parentFileDirectory = dirname(Domains.jsonschemafile)) 
 
-# For now exclude non lambert domains from test 
-for domain in filter(d->d ∉ ["SCANDINAVIA_ROTM","NORWAY_POLAR", "RCR_POLAR"], Domains.domains)     
+# For now exclude non lambert domains from tests 
+for domain in filter(d->d ∉ ["SCANDINAVIA_ROTM","NORWAY_POLAR", "RCR_POLAR"], Domains.domains) 
+
+    d = JSON.parsefile(joinpath(Domains.jsondir, "$domain.json"))      
+    
     @testset "$domain" begin 
-        # Validate agains JSON schema
-        d = JSON.parsefile(joinpath(Domains.jsondir, "$domain.json"))    
+
+        # Validate against JSON schema         
         @test JSONSchema.isvalid(d, schema) 
 
-        # Test that the North Pole is outside the domain with ezone. 
-        # North Pole
-        lonlatNP = [0, 90]
-        @test !in(lonlatNP, unmarshal(Domain, d), with_ezone = true)
-    end 
+        # Test that the North Pole [0, 90] is outside the domain with ezone. 
+        @test !in([0, 90], unmarshal(Domain, d), with_ezone = true)
 
-end # @testset "JSONSchema" begin   
+    end # @testset "$domain" begin  
+
+end  
 
